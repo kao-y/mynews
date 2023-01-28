b93d9de88866208ed7dc02984d5825a8a3fc91da
=ka.y <=ka.13251y@outlook.com>
2023-01-28 06:21:15 +0000

    text12

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 use App\Models\News;
 
+use App\Models\History;
+
+use Carbon\Carbon;
+
+
+
 class NewsController extends Controller
 {
     public function add()
@@ -89,6 +95,12 @@ class NewsController extends Controller
 
         // 該当するデータを上書きして保存する
         $news->fill($news_form)->save();
+        
+        // 以下を追記
+        $history = new History();
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
 
         return redirect('admin/news');
     }
